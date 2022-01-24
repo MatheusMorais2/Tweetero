f2da1e827d2b3ec834fc23d3019939536e6fe173
Matheus <morais.matheus.web@gmail.com>
2022-01-24 13:55:36 -0300

    Post and get tweets

--- tweteroo/tweteroo/API/index.js
+++ tweteroo/tweteroo/API/index.js
@@ -6,17 +6,30 @@ app.use(cors());
 app.use(express.json());
 
 let tweets = [];
-let user = [];
+let users = [];
 
 app.post("/sign-up", (req, res) => {
-  user.push(req.body);
+  users.push(req.body);
   res.send("OK");
 });
 
 app.post("/tweets", (req, res) => {
   tweets.push(req.body);
-  console.log(tweets);
   res.send("OK");
+});
+
+app.get("/tweets", (req, res) => {
+  let tweetsRes = [];
+
+  for (let i = tweets.length - 1; i > tweets.length - 11 && i >= 0; i--) {
+    const userAvatar = users.find(
+      (elem) => elem.username === tweets[i].username
+    ).avatar;
+
+    tweetsRes.push({ ...tweets[i], avatar: userAvatar });
+  }
+
+  res.send(tweetsRes);
 });
 
 app.listen(5000);
